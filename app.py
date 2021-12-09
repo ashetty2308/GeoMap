74747a634c65b53dd89391ec8f4882fa237de0cd
Adit Shetty <ashetty2308@gmail.com>
2021-12-08 21:48:18 -0500

    Adding a lot of UI elements + marker JS components

--- app.py
+++ app.py
@@ -1,22 +1,20 @@
 from logging import debug
 from flask import Flask, render_template
  
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static')
  
 @app.route("/")
 def index():
     return render_template("index.html")
  
-@app.route("/hi/")
+@app.route("/geomap/")
 def who():
-    return "testing hi!"
+    return render_template("index_2.html")
  
 @app.route("/hi/<username>")
 def greet(username):
     return f"Welcome, {username}!"
- 
-# run by going to terminal and -> py -m flask run
- 
+  
 if __name__ == "__main__":
     app.run(debug=True)
  
--- static/styles/main.js
+++ static/styles/main.js
@@ -1,36 +1,51 @@
 var count = 0;
+var clearClicked = false;
+var map = new L.map('map').setView([0, 0], 12);
+var marker = new L.map('marker').setView([0,0]);
 
-function generateMap(userLat, userLong){
- 
 
-}
 function userLatLong(){
+
   count++;
-  var latUser = document.getElementById("lat").value;
-  var longUser = document.getElementById("long").value;
 
-  var map = new L.map('map').setView([latUser, longUser], 12);
+  var userCity = document.getElementById("userCity").value;
+  let url = "http://api.openweathermap.org/geo/1.0/direct?q="+userCity+"&limit=5&appid=7e3eac24249fda8fc39ac0f291163bb2";
 
-  // destroyMap(map);
+  fetch(url).then(function (response) {
+     return response.json();    
+  }).then(function (obj){
 
-  // map = L.map('map').setView([latUser, longUser], 12);
+    city_lat= obj[0]["lat"];
+    city_long = obj[0]["lon"];
 
-  L.tileLayer('https://api.maptiler.com/maps/hybrid/{z}/{x}/{y}.jpg?key=fZY5lvXJupgHWwlCqMFd', {
-    attribution: '<a href="<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>'
-    }).addTo(map) 
+    if(count%2==1){
+      map = new L.map('map').setView([city_lat, city_long], 13);
+      marker = L.marker([city_lat, city_long], {title:userCity, fillOpacity: 0.5}).addTo(map);
+      marker.bindPopup(userCity).openPopup();
 
+      
+      L.circle([city_lat, city_long], {
+        color: 'yellow',
+        fillColor: 'green',
+        fillOpacity: 0.5,
+        radius: 500
+      }).addTo(map);
 
-  generateMap(latUser, longUser);
+    
+      L.tileLayer('https://api.maptiler.com/maps/hybrid/{z}/{x}/{y}.jpg?key=fZY5lvXJupgHWwlCqMFd', {
+      attribution: '<a href="<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>'
+      }).addTo(map) 
+    }
+    else{
+      destroyMap(map);
+    }
+  })
 }
+
 function destroyMap(map){
   map.remove();
+  userLatLong();
 }
 
-  /*
-  const api_request = new XMLHttpRequest();
-  api_request.open("GET", "http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=7e3eac24249fda8fc39ac0f291163bb2")
+// wikipedia api --> https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=Austin&format=json
 
-  alert(latUser);
-
-   registering : DONE 
-    */
