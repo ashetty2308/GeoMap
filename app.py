079fbae482a587a9bd6a4085a1bc9ff71674f9eb
Adit Shetty <ashetty2308@gmail.com>
2021-12-10 21:02:37 -0500

    working on GeoMap search engine HTML/CSS

--- app.py
+++ app.py
@@ -10,13 +10,6 @@ def index():
 @app.route("/geomap/")
 def who():
     return render_template("index_2.html")
- 
-@app.route("/hi/<username>")
-def greet(username):
-    return f"Welcome, {username}!"
-  
-
-
   
 if __name__ == "__main__":
     app.run(debug=True)
